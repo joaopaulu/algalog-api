3545d2836b90ee83041b34dea2d6ea93528a4da8
João Paulo <jptick@gmail.com>
2021-05-19 09:37:37 -0300

    Using Jakarta Persistence JPA

--- src/main/java/com/algaworks/algalog/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/model/Cliente.java
@@ -1,14 +1,18 @@
 package com.algaworks.algalog.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String telefone;
+
 }
